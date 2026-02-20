d0410dca8c797b4d4bc9a43c633efcc40144050c
João Pedro Almeida <joaopedrowilliam41@gmail.com>
2026-02-19 21:34:51 -0300

    Começando o curso de JavaScript

---
+++ 4_variaveis/js/scripts.js
@@ -0,0 +1,31 @@
+var teste = 1;
+
+var nome = "João";
+
+console.log(teste);
+
+console.log('Mathues');
+
+console.log(teste);
+
+console.log(nome);
+
+var $nome = 'ASD'
+var _nome = 'ASD2'
+
+console.log($nome);
+console.log(_nome);
+
+var nome5 ='ASD3';
+
+console.log(nome5);
+
+var meuPrimeiroNome = 'João'; // CamelCase
+
+console.log(meuPrimeiroNome);
+
+var meuNome;
+
+meuNome = "JoãoP";
+
+console.log(meuNome);
