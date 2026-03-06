a0aa48bc146c7bd31f07054364eaada7f84f4a44
João Pedro Almeida <joaopedrowilliam41@gmail.com>
2026-03-06 19:57:18 -0300

    Aualas dos 05 e 06/03
    Organizei a pasta de teste e fiz alguns testes

---
+++ 19_op_atribuicao/js/scripts.js
@@ -0,0 +1,44 @@
+var x = 1;
+var y = 2;
+// os operadores mais utilizados no JS
+
+
+
+/*
+Operador	  O que faz
+  +=	      soma
+  -=	      subtrai
+  *=	      multiplica
+  /=	      divide
+  
+*/
+
+// soma 
+
+console.log(x = x +y);
+console.log(x += y);
+
+// subtração
+
+console.log(x -= y);
+
+// Multiplicação
+
+console.log(x *= y);
+
+// divisão
+
+conso.log(x /= y);
+
+// loops
+console.log(x ++);
+console.log(x--);
+
+
+while(x <= 100){
+  console.log(x);
+  
+  x *=2;
+}
+
+
---
+++ 22_function/js/scripts.js
@@ -0,0 +1,14 @@
+function primeiraFuncao(){
+  console.log("Esse é minha primeira função")
+}
+
+primeiraFuncao();
+
+function listaeNomes(nomes){
+  console.log("João");
+  console.log("Pedro");
+  console.log("William");
+
+}
+
+listaeNomes();
